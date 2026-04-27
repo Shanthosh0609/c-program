a45fdf3ac21dae477458a85fb6f8fef98f2ca147
Shanthosh0609 <sshanthoshp@gmail.com>
2026-04-27 11:45:22 +0530

    Create salaryunion.c

---
+++ Ex9/salaryunion.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+union employee
+{
+   char name[20];
+   int empno;
+   int bp;
+};
+int main()
+{
+   int i,n;
+   float ts;
+   union employee c[60];
+   printf("enter no of emplyees");
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+      printf("\n enter the name \t");
+      scanf("%s",c[i].name);
+      printf("\n enter the no \t");
+      scanf("%d",&c[i].empno);
+      printf("\n enter the payment \t");
+      scanf("%d",&c[i].bp);
+   }
+   printf("employee details are");
+   for(i=0;i<n;i++)
+   {
+      ts=c[i].bp+0.5*c[i].bp+0.05*c[i].bp;
+      printf("\n total \t %f",ts);
+   }
+}
