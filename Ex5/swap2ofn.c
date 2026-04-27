0dafdac28311789d065946e860ead43243e31036
Shanthosh0609 <sshanthoshp@gmail.com>
2026-04-27 11:38:48 +0530

    Create swap2ofn.c

---
+++ Ex5/swap2ofn.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void swap(int,int);
+int main()
+{
+   int a,b;
+   printf("enter the elements");
+   scanf("%d%d",&a,&b);
+   printf("before swapping the values in main a=%d,b=%d\n",a,b);
+   swap(a,b);
+   printf("after swapping values in main a=%d,b=%d\n",a,b);
+}
+void swap(int a,int b)
+{
+   int temp;
+   temp=a;
+   a=b;
+   b=temp;
+   printf("after
