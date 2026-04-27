9c2aa0a7e0909ee70c2f503b1835ec2f5e32297c
Shanthosh0609 <sshanthoshp@gmail.com>
2026-04-27 11:27:23 +0530

    Create vowels and consonant.c

---
+++ Ex2/vowels and consonant.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+char main()
+{
+   char vowel;
+   printf("Enter any character :");
+   scanf("%c",&vowel);
+   switch (vowel)
+   {
+   case'a':
+          printf(" Entered character is a Vowel");
+          break;
+   case'e':
+         printf(" Entered character is a Vowel");
+         break;
+   case'i':
+         printf(" Entered character is a Vowel");
+         break;
+   case'o':
+         printf(" Entered character is a Vowel");
+         break;
+   case'u':
+         printf(" Entered character is a Vowel");
+         break;
+   case'A':
+         printf(" Entered character is a Vowel");
+         break;
+   case'E':
+         printf(" Entered character is a Vowel");
+         break;
+   case'O':
+         printf(" Entered character is a Vowel");
+         break;
+   case'I':
+         printf(" Entered character is a Vowel");
+         break;
+   case'U':
+         printf(" Entered character is a Vowel");
+         break;
+   default:
+         printf("Entered character is a Consonant");
+   }
+   return 0;
+}
+
